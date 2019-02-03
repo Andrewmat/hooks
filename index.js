7091d50ba41ea147095ea09641fd1d40222c6aae
Andrewmat <andre.m.santos1@gmail.com>
2019-02-03 18:09:46 -0200

    Fix imports

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import CacheProvider from './components/CacheProvider'
-import useCache from './hooks/useCache'
-import useCounter from './hooks/useCounter'
-import useDocumentTitle from './hooks/useDocumentTitle'
-import useIterator from './hooks/useIterator'
-import useToggle from './hooks/useToggle'
+import CacheProvider from './src/components/CacheProvider'
+import useCache from './src/hooks/useCache'
+import useCounter from './src/hooks/useCounter'
+import useDocumentTitle from './src/hooks/useDocumentTitle'
+import useIterator from './src/hooks/useIterator'
+import useToggle from './src/hooks/useToggle'
 
 export default {
   CacheProvider,
